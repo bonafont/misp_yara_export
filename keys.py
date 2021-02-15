be2d1e146f6928befd7531cb21816f382b50a8cb
OGLAVIE <anthonyfey@gmail.com>
2021-02-15 11:59:13 +0100

    Initial commit

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+misp_url = '' ## The MISP server address
+misp_key = '' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_verifycert = False
+misp_client_cert = ''
+misp_verify = False
+proofpoint_key = 'Your Proofpoint TAP auth key'
